6e74b83830f8c899c7572a14641d504085781482
Chirag Ahuja <chirag@tatrasdata.com>
2020-07-20 18:11:30 +0530

    Added test cases two day 2 solution

--- #100daysOfCode/day2.py
+++ #100daysOfCode/day2.py
@@ -1,26 +1,40 @@
+import unittest
+
 def threeNumberSum(array, targetSum):
-    # Write your code here.
-    array = sorted(array)
-    n = len(array)
-    
-    result = []
-    
-    for idx in range(n-2):
-      num1 = array[idx]
-      start = idx+1
-      end = n-1
-      temp_target = targetSum - num1
-      while start<end:
-        if temp_target == array[start]+array[end]:
-          result.append(sorted([num1, array[start], array[end]]))
-          start+=1
-        elif temp_target > array[start]+array[end]:
-          start+=1
-        else:
-          end-=1
-    return result
-if __name__ == '__main__':
-  array =  [12, 2, 1, 3, -6, 5, -8, 6]
-  targetSum = 0
-  print(threeNumberSum(array, targetSum))
+	array.sort()
+	n = len(array)
+	triplets = []
+	for i in range(n-2):
+		left = i + 1
+		right = n -1
+		
+		while left < right:
+			currentSum = array[i] + array[left] + array[right]
+			if currentSum == targetSum:
+				triplets.append([array[i], array[left], array[right]])
+				left += 1
+				right -= 1
+			elif currentSum < targetSum:
+				left +=1
+			elif currentSum > targetSum:
+				right -=1
+	return triplets
   
+class TestProgram(unittest.TestCase):
+    def assertListEquals(self, l1, l2):
+        return sorted(l1) == sorted(l2)
+
+    def test_case_1(self):
+      self.assertListEquals(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0), [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]])
+      
+    def test_case_2(self):
+        self.assertEquals(threeNumberSum([1, 2, 3], 6), [[1, 2, 3]])
+
+    def test_case_3(self):
+        self.assertEquals(threeNumberSum([1, 2, 3], 7), [])
+
+    def test_case_4(self):
+        self.assertEquals(threeNumberSum([8, 10, -2, 49, 14], 57), [[-2, 10, 49]])
+  
+if __name__ == "__main__":
+  unittest.main()
